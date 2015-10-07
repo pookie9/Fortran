671493b05a46fbdc4cf2890ccb7268eb413470ae
MIDN JOSHUA KING {SCS} <m163468@usna.edu>
2015-10-06 22:16:34 -0400

    Better commenting on proj.f

--- part1/proj.f90
+++ part1/proj.f90
@@ -1,15 +1,18 @@
-!  F90 (Fortran 90) written to fulfill Part 1 for Programming Languages
+!  F90 (Fortran 90) written by King and Groves to fulfill Part 1 for Programming Languages
 !  Does Parts A,B,C,D and G
 
 program bottles
-  implicit none
+  implicit none !Used to say that implicit type casting is now allowed
+
+  !Note that all variable declarations must be done at the top of the program
   integer::i,numBottles
-  integer,dimension(:),allocatable::factors
-  character (len=*),parameter::line1='() line of text on the screen, () line of text.', & !& is used to continue the same statement on next line
-  beg1end2=' lines of text on the screen, ',end1=' lines of text.',beg2='Print it out, stand up and shout, '
-  100 format (7x,'Name:', 7x, 'Id:', 1x, 'Weight:')
+  !The (len=*) means to calculate the length of the string from its literal value
+  character (len=*),parameter::line1='() line of text on the screen, () line of text.',& ! & is used to have statements wrap around
+ beg1end2=' lines of text on the screen, ',end1=' lines of text.',beg2='Print it out, stand up and shout, '
+
+  !in writes and reads the first * means to write to stdout/read from stdin, the second means to use auto-formatting
+  write(*,*)"Enter number of lines to print: "
   read (*,*)numBottles
-  print 100
   if (numBottles == 1) then
      write(*,*)line1
   endif
@@ -17,16 +20,18 @@ program bottles
      if (i == 1) then
         write(*,*),line1
      else
-        call primeFactors(i)
-        write(*,"(a30)",advance="no"),beg1end2
-        call primeFactors(i)
+        call primeFactors(i)!This will print out all of the prime factors on the same line with the parenthesis around them
+        !The (a30) is formatting a means string, 30 means it will print out a string of length 30, truncating or adding spaces
+        !as need be in order to make the length 30, advance="no" means to not print out a newline character
+        write(*,"(a30)",advance="no"),beg1end2 
+        call primeFactors(i)!This will print out all of the prime factors on the same line with the parenthesis around them
         write(*,*),end1
-     end if
+     endif
      write(*,"(a35)",advance="no"),beg2
      call primeFactors(i+1)
-     write(*,*),beg1end2
-  end do
-end program bottles
+     write(*,*),beg1end2 ! Note the lack of advance="no" so it will print out a newline character
+  enddo
+endprogram bottles
 
 !Writes out the factors of n
 subroutine primeFactors(n)
@@ -38,6 +43,10 @@ subroutine primeFactors(n)
   do while (m>=i)
      !does not support % for modular division, have to use mod(a,b) instead to get the equivalent of a%b
      if (mod(m, i)==0) then ! i is a factor of n, now begins the wacky formatting to make i go pretty
+        !the i means to print an integer, the 5 means 5 digits, if it goes over 5 then it will just print **, 
+        !otherwise it will pad with spaces, so if you give it a number with a factor > 5 digits, this won't work
+        !But it still looks stupid this way because if often has 4 extra spaces, there is just no way to do pretty formatting
+        !in Fortran, it is literally not possible
         write(*,"(i5)",advance="no"), i
         m=m/i
      else
